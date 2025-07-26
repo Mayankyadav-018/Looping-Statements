0668077a150a05b218da4480e334891c92c8fc5b
Mayank Yadav <mayankyadav18101@gmail.com>
2025-07-26 22:16:41 +0530

    Create Program to print even values between 0 and 10.cpp

---
+++ Program to print even values between 0 and 10.cpp
@@ -0,0 +1,25 @@
+/* Name - Mayank yadav
+   PRN - 24070123060
+   Class - ENTC A'3' */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Even values between 0 and 10:"<<endl;
+    for(int i=1;i<10;i++){
+        if(i%2==0){
+            cout << i << endl;
+        }
+    }
+return 0;
+}
+
+
+
+/* OUTPUT:
+Even values between 0 and 10:
+2
+4
+6
+8 */
