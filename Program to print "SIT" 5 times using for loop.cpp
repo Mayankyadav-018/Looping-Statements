1f360548268bf5031a0810d542141010e1ab2f09
Mayank Yadav <mayankyadav18101@gmail.com>
2025-07-26 23:33:19 +0530

    Create Program to print "SIT" 5 times using for loop.cpp

---
+++ Program to print "SIT" 5 times using for loop.cpp
@@ -0,0 +1,24 @@
+/* Name - Mayank yadav
+   PRN - 24070123060
+   Class - ENTC A'3' */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a;
+    a = "SIT";
+    for(int i=0;i<5;i++){
+        cout << a << endl;
+    }
+    return 0;
+}
+
+
+
+/* OUTPUT:
+SIT
+SIT
+SIT
+SIT
+SIT      */
