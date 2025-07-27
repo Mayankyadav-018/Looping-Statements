721de07de9430e3fa57913caeb6f533223f80f0e
Mayank Yadav <mayankyadav18101@gmail.com>
2025-07-27 15:09:17 +0530

    Create Program using nester for loop.cpp

---
+++ Program using nester for loop.cpp
@@ -0,0 +1,30 @@
+/* NAME  - MAYANK YADAV
+   PRN   - 24070123060
+   CLASS - ENTC A-3
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //OUTER LOOP
+    for(int i = 1; i <= 2; ++i) {
+        cout <<"Outer: " << i << "\n"; 
+        //INNER LOOP
+        for(int j = 1; j <= 3; ++j) {
+            cout << " Inner: " << j << "\n";}
+}
+return 0;
+}    
+
+
+/* OUTPUT 
+Outer: 1
+ Inner: 1
+ Inner: 2
+ Inner: 3
+Outer: 2
+ Inner: 1
+ Inner: 2
+ Inner: 3    */
+           
