45267a707d816533555c0ee7eea4a45da4aa5523
Mayank Yadav <mayankyadav18101@gmail.com>
2025-07-27 15:34:05 +0530

    Create Program to create a Continuous Alphabet Pyramid.cpp

---
+++ Program to create a Continuous Alphabet Pyramid.cpp
@@ -0,0 +1,32 @@
+/* NAME  - MAYANK YADAV
+   PRN   - 24070123060
+   CLASS - ENTC A-3
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch = 'A';
+    int rows = 4;   
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+
+/* OUTPUT 
+A 
+B C 
+D E F 
+G H I J   */
