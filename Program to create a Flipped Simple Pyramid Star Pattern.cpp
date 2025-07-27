9b756c3f5d2c640d667718d1c29c04e59e925ce3
Mayank Yadav <mayankyadav18101@gmail.com>
2025-07-27 15:23:43 +0530

    Create Program to create a Flipped Simple Pyramid Star Pattern.cpp

---
+++ Program to create a Flipped Simple Pyramid Star Pattern.cpp
@@ -0,0 +1,33 @@
+/* NAME  - MAYANK YADAV
+   PRN   - 24070123060
+   CLASS - ENTC A-3
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j,k;
+    int n=5;
+    for(i = 1; i<=n;i++){
+        
+        for(j=1;j<n-i+1;j++){
+            cout << " ";
+        }
+            for(k=1;k<=i;k++){
+                cout << "*";
+        }
+        cout << endl;
+    }
+    
+
+    return 0;
+}
+
+
+/* OUTPUT
+    *
+   **
+  ***
+ ****
+*****    */
